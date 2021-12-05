8fb3ab2685e3102124de6a8397b81b46ad328e4d
Jared Tamulynas <jwtamuly@ncsu.edu>
2021-12-05 18:58:32 -0500

    Adding project

---
+++ 13 - Instafilter/Instafilter/Instafilter/ImageSaver.swift
@@ -0,0 +1,18 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by Jared Tamulynas on 12/5/21.
+//
+
+import SwiftUI
+
+class ImageSaver: NSObject {
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
+    }
+
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        print("Save finished!")
+    }
+}
---
+++ 13 - Instafilter/Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Jared Tamulynas on 12/3/21.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
