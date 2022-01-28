5e4ad14f5b3c79a12f164c2c916fbc0477c9896d
Jared Tamulynas <jwtamuly@ncsu.edu>
2022-01-28 14:54:12 -0500

    Adding project

---
+++ 16 - HotProspects/HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Jared Tamulynas on 1/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selectedTab = "One"
+    
+    @StateObject var prospects = Prospects()
+
+    var body: some View {
+        TabView {
+            ProspectsView(filter: .none)
+                .tabItem {
+                    Label("Everyone", systemImage: "person.3")
+                }
+            ProspectsView(filter: .contacted)
+                .tabItem {
+                    Label("Contacted", systemImage: "checkmark.circle")
+                }
+            ProspectsView(filter: .uncontacted)
+                .tabItem {
+                    Label("Uncontacted", systemImage: "questionmark.diamond")
+                }
+            MeView()
+                .tabItem {
+                    Label("Me", systemImage: "person.crop.square")
+                }
+        }
+        .environmentObject(prospects)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 16 - HotProspects/HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Jared Tamulynas on 1/3/22.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
