f0856b310b3666c8e9bf5d82b87f75a08870062b
Jared Tamulynas <jwtamuly@ncsu.edu>
2022-02-07 10:00:16 -0500

    adding project

---
+++ 18 - LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Jared WIlliam Tamulynas on 2/6/22.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
